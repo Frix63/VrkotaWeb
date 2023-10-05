2088e34f5783840fa0964327d2e1eedc032d4189
Chris <FrixLP63@gmail.com>
2023-10-05 14:47:18 +0000

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	import Image from '$lib/icons/Image.svelte';
+	import Burgir from '$lib/icons/burgir.svelte';
+</script>
+
+<div class=" flex place-content-center w-screen bg-[#ff8482]">
+	<div class=" lg:w-[calc(100%-270px)] w-[calc(100%-30px)]">
+		<div class=" h-[80px] grid content-center w-full">
+			<!-- Mobile -->
+			<div class=" flex lg:hidden w-full">
+				<div class=" grow font-Alfo h-full text-white text-[40px] leading-[48px]">Logo</div>
+				<div class=" grow place-content-end content-center h-full grid"><Burgir /></div>
+			</div>
+			<!-- Desktop -->
+			<div class=" hidden lg:flex w-full">
+				<div class=" grow font-Alfo h-full text-white text-[40px] leading-[48px]">Logo</div>
+				<div class=" grid content-center grid-flow-col h-full text-center">
+					<div class=" font-DM text-white m-4">Portfolio</div>
+					<div class=" font-DM text-white m-4">Contact</div>
+				</div>
+				<div class=" grow place-content-end content-center h-full grid"><Burgir /></div>
+			</div>
+		</div>
+		<!-- Mobile -->
+		<div class=" block lg:hidden w-full">
+			<div class=" text-white font-[20px] font-DM font-bold">Delectus instructior</div>
+			<div class=" font-Alfo text-[56px] text-white">Vide legim- us consec- tetuer</div>
+			<input
+				type="text"
+				placeholder="Email"
+				class=" p-4 h-[48px] w-full border-[1px] border-[#DADADA] rounded-[4px] mb-6"
+			/>
+			<button class=" w-1/2 h-[48px] bg-white border-[2px] border-black rounded-[4px]"
+				>Submit</button
+			>
+			<Image />
+		</div>
+		<!-- Desktop -->
+	</div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      'Alfo': ['Alfa Slab One', 'cursive'],
+      'DM': ['DM Sans', 'sans-serif'],
+    },
+    extend: {}
+  },
+  plugins: []
+};
